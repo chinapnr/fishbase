5c97055bb1672d691799cbda20ef88718f6118a7
yijun_mbp15 <wingfish@gmail.com>
2016-03-31 15:26:53 +0800

    2016.3.31

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fish_base',
+    version='1.0.0',
+    packages=['fish_base'],
+    url='http://chuangyiji.com',
+    license='',
+    author='david.yi',
+    author_email='wingfish@gmail.com',
+    description='some useful functions for python'
+)
---
+++ tests/example_fish_base.py
@@ -0,0 +1,4 @@
+import fish_base
+
+
+print(fish_base.get_md5('Hello World'))
