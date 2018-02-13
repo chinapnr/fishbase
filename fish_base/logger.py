ae37672955bdfaa46384b824e9f41aa7febae16a
yijun_home <wingfish@gmail.com>
2018-02-13 22:00:52 +0800

    2018.2.13 edit logger, remove thread watch

--- fish_base/logger.py
+++ fish_base/logger.py
@@ -13,13 +13,14 @@ logger = logging.getLogger()
 # 设置日志记录，按照每天一个文件，记录包括 info 以及以上级别的内容
 # 输入: local_file 日志文件名
 # 2018.2.6 edit by David Yi
-# 2018.2.11 log 相关代码优化简化; #11010
+# 2018.2.11 edit, log 相关代码优化简化; #11010
+# 2018.2.13 edit, remove thread watch
 def set_log_file(local_file=None):
 
     default_log_file = 'default.log'
 
     _formatter = logging.Formatter(
-        '%(asctime)s %(levelname)s %(threadName)s[%(thread)d] %(filename)s[line:%(lineno)d] %(message)s')
+        '%(asctime)s %(levelname)s %(filename)s[line:%(lineno)d] %(message)s')
 
     if local_file is not None:
         default_log_file = local_file
