b5a108d0d49b1aa2709164287462c8d15faf0381
yijun_mbp15 <wingfish@gmail.com>
2018-06-03 17:15:44 +0800

    2018.6.3 small edit

--- docs/_build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '1.0.12',
-    LANGUAGE: 'zh-CN',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
