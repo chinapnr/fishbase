84d470f1962abb2321e41d8597b5faf4914c4a24
Chunying Jia <chunying.jia@chinapnr.com>
2018-06-27 21:44:09 +0800

    2018.6.27 v1.0.14, add fish_csv unittest #72

---
+++ test/test_csv.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+from fishbase.fish_csv import csv_file_to_list
+
+
+# 2018.6.27 v1.0.14 #73 create by Jia ChunYing
+class TestCsv(object):
+
+    # 测试 csv_file_to_list()  tc
+    def test_csv(self):
+        # 创建csv测试文件
+        csv_file_name = '.test_cache_data/test_file.csv'
+        if os.path.isfile(csv_file_name):
+            os.remove(csv_file_name)
+        if not os.path.exists(os.path.dirname(csv_file_name)):
+            os.makedirs(os.path.dirname(csv_file_name))
+        csv_content = "a,b\n1,2"
+        with open(csv_file_name, 'w') as f:
+            f.write(csv_content)
+            f.close()
+        result = csv_file_to_list(csv_file_name)
+        shutil.rmtree(os.path.dirname(csv_file_name))
+        assert len(result) == 2
+        assert len(result[0]) == 2
+        assert len(result[1]) == 2
