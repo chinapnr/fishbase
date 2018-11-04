072f8d558d8fc192798e6c63e755ee953afad876
mindjun.hu <mindjun.hu@gmail.com>
2018-11-04 17:44:45 +0800

    20181104： v1.0.3 update unitetst for get_years

--- test/test_date.py
+++ test/test_date.py
@@ -15,21 +15,11 @@ class TestFishDate(object):
     # test get_years() tc
     def test_get_years_01(self):
         this_month = datetime.date(day=1, month=6, year=2018)
-        
+
         assert get_years(7, this_month) == '201901'
         assert get_years(-5, this_month) == '201801'
         assert get_years(-6, this_month) == '201712'
 
-        this_month1 = datetime.datetime.now()
-        
-        y = this_month1.year
-        m = this_month1.month + 1
-        if m == 12:
-            y += 1
-            m = 1
-        
-        assert get_years(1) == ''.join(['%04d' % y, '%02d' % m])
-        
         with pytest.raises(TypeError):
             get_years(-5, 8)
 
