d89ba514bbe0eb1021028c87493ac76ee5947d29
yijun_mini <wingfish@gmail.com>
2018-12-16 22:13:39 +0800

    2018.12.16 v1.1.4 #139 fish_data, 输入身份证的省份、城市、地区信息，返回地区编号，基本完成，ut，demo，docstring

--- demo/demo_data.py
+++ demo/demo_data.py
@@ -28,3 +28,51 @@ print(id2, is_valid_id_number(id2)[0])
 
 print('---')
 
+
+# 2018.12.16 edit by David Yi
+print('--- fish_data get_zonecode_by_area demo ---')
+
+result = get_zonecode_by_area(area_str='北京市')
+print(result)
+
+result = get_zonecode_by_area(area_str='上海市')
+print(result)
+
+# 精确查询，参数不省略
+result = get_zonecode_by_area(area_str='北京市', match_type='EXACT')
+print(result)
+
+# 模糊查询
+result = get_zonecode_by_area(area_str='北京市', match_type='FUZZY')
+print(result)
+
+# 模糊查询，转换为地区码列表
+result = get_zonecode_by_area(area_str='西安市', match_type='FUZZY')
+print(result)
+
+result0 = []
+for i in result:
+    result0.append(i[0])
+
+print('---西安市---')
+print(len(result0))
+print(result0)
+
+# 模糊查询
+result = get_zonecode_by_area(area_str='西安', match_type='FUZZY')
+print(result)
+
+# 模糊查询, 结果返回设定 list
+result = get_zonecode_by_area(area_str='西安', match_type='FUZZY', result_type='LIST')
+print(result)
+
+# 模糊查询, 结果返回设定 single_str
+result = get_zonecode_by_area(area_str='西安市', match_type='FUZZY', result_type='SINGLE_STR')
+print(result)
+
+# 模糊查询, 结果返回设定 single_str，西安市 和 西安 的差别
+result = get_zonecode_by_area(area_str='西安', match_type='FUZZY', result_type='SINGLE_STR')
+print(result)
+
+
+print('---')
--- test/test_data.py
+++ test/test_data.py
@@ -3,7 +3,7 @@ from fishbase.fish_data import *
 
 
 # 2018.12.10 v1.1.3 create by Jia ChunYing
-# 2018.12.13 v1.1.4 edit by David Yi
+# 2018.12.13 12.16 v1.1.4 edit by David Yi
 class TestData(object):
 
     def test_get_idcard_checkcode(self):
@@ -36,3 +36,52 @@ class TestData(object):
         # id number is int
         id3 = 130522198407316471
         assert is_valid_id_number(id3)[0] is True
+
+    # 2018.12.16 edit by David Yi
+    def test_get_zonecode_by_area(self):
+        # area_str，基本测试
+        values = [('110000', '北京市')]
+        assert get_zonecode_by_area('北京市') == values
+
+        # area_str，显示设定参数
+        result = get_zonecode_by_area(area_str='上海市')
+        values = [('310000', '上海市')]
+        assert result == values
+
+        # area_str, match_type = EXACT 精确
+        result = get_zonecode_by_area(area_str='北京市', match_type='EXACT')
+        values = [('110000', '北京市')]
+        assert result == values
+
+        # area_str, match_type = FUZZY 模糊
+        result = get_zonecode_by_area(area_str='北京市', match_type='FUZZY')
+        values = [('110000', '北京市'), ('110100', '北京市市辖区'), ('110101', '北京市东城区'), ('110102', '北京市西城区'), ('110103', '北京市崇文区'), ('110104', '北京市宣武区'), ('110105', '北京市朝阳区'), ('110106', '北京市丰台区'), ('110107', '北京市石景山区'), ('110108', '北京市海淀区'), ('110109', '北京市门头沟区'), ('110111', '北京市房山区'), ('110112', '北京市通州区'), ('110113', '北京市顺义区'), ('110114', '北京市昌平区'), ('110115', '北京市大兴区'), ('110116', '北京市怀柔区'), ('110117', '北京市平谷区'), ('110200', '北京市市辖县'), ('110221', '北京市昌平县')]
+        assert result == values
+
+        # area_str, match_type 模糊, result_type=LIST 列表
+        result = get_zonecode_by_area(area_str='西安市', match_type='FUZZY', result_type='LIST')
+        values = [('610100', '陕西省西安市'), ('610101', '陕西省西安市市辖区'), ('610102', '陕西省西安市新城区'), ('610103', '陕西省西安市碑林区'), ('610104', '陕西省西安市莲湖区'), ('610111', '陕西省西安市灞桥区'), ('610112', '陕西省西安市未央区'), ('610113', '陕西省西安市雁塔区'), ('610114', '陕西省西安市阎良区'), ('610115', '陕西省西安市临潼区'), ('610116', '陕西省西安市长安区')]
+        assert result == values
+
+        # area_str, match_type 精确, result_type=LIST 列表
+        result = get_zonecode_by_area(area_str='北京市', match_type='EXACT', result_type='LIST')
+        values = [('110000', '北京市')]
+        assert result == values
+
+        # area_str, match_type 精确, result_type=SINGLE_STR 字符串
+        result = get_zonecode_by_area(area_str='北京市', match_type='EXACT', result_type='SINGLE_STR')
+        values = '110000'
+        assert result == values
+
+        # area_str, 结果大于20个，返回20个
+        result = len(get_zonecode_by_area(area_str='市', match_type='FUZZY'))
+        assert result == 20
+
+        # area_str, match_type 精确, result_type=SINGLE_STR 字符串, 无结果返回
+        result = get_zonecode_by_area(area_str='美国', match_type='EXACT', result_type='SINGLE_STR')
+        values = ''
+        assert result == values
+
+
+
+
